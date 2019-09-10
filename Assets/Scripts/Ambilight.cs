061986d7fcb56c442076925e0694db59bf1d6bda
masterrey <masterrey@gmail.com>
2019-09-10 16:48:54 -0300

    Cena arrumada do cinema

--- Assets/Scripts/Ambilight.cs
+++ Assets/Scripts/Ambilight.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Ambilight : MonoBehaviour
 {
+
+   
+    Material mymat;
+    Rect rectReadPicture;
+    public Texture2D texture;
+    public RenderTexture renderTexture;
+    int count_x = 8 * 10;
+    int count_y = 8 * 10;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+        texture = new Texture2D(count_x, count_y, TextureFormat.RGB24, false);
+
+        Rect rectReadPicture = new Rect(0, 0, count_x, count_y);
+
+        
+
+        mymat = GetComponent<Renderer>().material;
+        //mymat.SetColor("_EmissiveColor", Color.red);
+
+
+    }
+   
+    
+    
     // Update is called once per frame
     void Update()
     {
-        
+        RenderTexture.active = renderTexture;
+        texture.ReadPixels(rectReadPicture, 0, 0);
+       Color c= texture.GetPixel(0,0);
+        mymat.SetColor("_EmissiveColor", c);
+
+
     }
 }
