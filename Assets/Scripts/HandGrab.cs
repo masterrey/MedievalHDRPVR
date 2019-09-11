3eaae817d14a1c54ea764c4b8f334b367857c3bb
Reinaldo Ramos <masterrey@gmail.com>
2019-09-10 21:16:53 -0300

    Grab de objetos

---
+++ Assets/Scripts/HandGrab.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGrab : MonoBehaviour
+{
+  
+    Rigidbody thing;
+    FixedJoint joint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<FixedJoint>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (thing != null && Input.GetButtonDown("Fire1") && thing.CompareTag("Grab"))
+        {
+            if (joint == null)
+            {
+                joint= thing.gameObject.AddComponent<FixedJoint>();
+                joint.breakForce = 200;
+                joint.connectedBody = GetComponent<Rigidbody>(); ;
+            }
+        }
+        if (thing != null && Input.GetButtonUp("Fire1") && thing.CompareTag("Grab"))
+        {
+            Destroy(joint);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grab"))
+        {
+            Rigidbody rdb = other.gameObject.GetComponent<Rigidbody>();
+            if (rdb)
+            {
+                thing = rdb;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (thing!=null && other == thing.gameObject)
+        {
+            Destroy(joint);
+            thing = null;
+        }
+    }
+}
