b1e7e2b6f3f40624063f4129679baafd29f866ba
masterrey <masterrey@gmail.com>
2019-09-23 17:27:04 -0300

    caneca e assets da melies

--- Assets/Scripts/SimpleHand.cs
+++ Assets/Scripts/SimpleHand.cs
@@ -26,6 +26,8 @@ public class SimpleHand : MonoBehaviour
             if (Input.GetButtonDown("Fire1"))
             {
                 Rigidbody rdb = hit.collider.gameObject.GetComponent<Rigidbody>();
+                if (!rdb)
+                     rdb = hit.collider.gameObject.GetComponentInParent<Rigidbody>();
                 if (rdb)
                 {
                     rdb.AddForceAtPosition(transform.forward* forcedirection, hit.point, ForceMode.Impulse);
