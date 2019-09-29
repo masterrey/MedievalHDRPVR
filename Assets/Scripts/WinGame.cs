58a74e4bda2d2c611f17279ae7142ff4a0e7c1a2
masterrey <masterrey@gmail.com>
2019-09-29 00:39:00 -0300

    bugs

--- Assets/Scripts/WinGame.cs
+++ Assets/Scripts/WinGame.cs
@@ -8,7 +8,7 @@ public class WinGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("LoseGame", 90);
+        Invoke("LoseGame", 100);
 
     }
 
